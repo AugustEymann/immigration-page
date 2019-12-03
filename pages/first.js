a7815e57d0d4028c1da18b231fae78c742365c80
August Eymann <august.eymann@gmail.com>
2019-12-03 09:20:38 -0800

    Carsouel??!

--- pages/first.js
+++ pages/first.js
@@ -8,3 +8,33 @@ $('.app').append(ejs.rr('components/header.ejs', {title: 'Immigrants', subtitle:
 $('.app').append(ejs.rr('views/home.ejs'))
 //Load Footer Last
 $('.app').append(ejs.rr('components/footer.ejs'));
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {  
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].styles.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
