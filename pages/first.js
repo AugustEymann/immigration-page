4726bc0be55015abe89c371e764ec9cf1b0bbc84
Maddox Keysor <code08@scrop.cruziohost.com>
2019-12-03 19:51:53 -0800

    Carsouel Changes

--- pages/first.js
+++ pages/first.js
@@ -3,7 +3,7 @@ $('.app').prepend(ejs.rr('components/head.ejs', {page: 'Home'}))
 $('.app').prepend(ejs.rr('components/navbar.ejs', {page: 'home'}))
 
 //Load Page Header
-$('.app').append(ejs.rr('components/header.ejs', {title: 'Immigrants', subtitle: 'Background'}))
+//$('.app').append(ejs.rr('components/header.ejs', {title: 'Immigrants', subtitle: 'Background'}))
 //Loads Page Data
 $('.app').append(ejs.rr('views/home.ejs'))
 //Load Footer Last
